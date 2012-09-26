3833fedf538dfd776e3446e2d0bb7762a37f2ee3
Felix Ding <felixding@gmail.com>
2012-09-26 14:19:53 +0800

    rename widgets_helper to jap_mag_widgets_helper

--- app/helpers/jap_mag_widgets_helper.rb
+++ app/helpers/jap_mag_widgets_helper.rb
@@ -1,4 +1,4 @@
-module WidgetsHelper
+module JapMagWidgetsHelper
 
   #
   # navigation helper
@@ -143,6 +143,20 @@ module WidgetsHelper
   def last_deployed_at
     file = File.join("tmp", "restart.txt")
     File.exists?(file) ? File.atime(file) : nil
+  end
+  
+  def current_controller_action_in?(*args)
+    args.each do |element|
+      if element.include?("#")
+        array = element.match(/([a-z\-\_]*)#([a-z\-\_]*)/).to_a
+        c, a = array[1], array[2]
+        return true if controller.controller_name == c && controller.action_name == a
+      else
+        return true if controller.controller_name == element
+      end
+    end
+    
+    false
   end
 
   protected
