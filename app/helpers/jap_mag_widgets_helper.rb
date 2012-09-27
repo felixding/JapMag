c2d4de218e39d8ec4ade2710a18332b9a51eda48
Felix Ding <felixding@gmail.com>
2012-09-27 10:19:06 +0800

    add get_body_id, get_body_class and is_mobile_device? helper methods, add '.Mobile' class to the default layout for mobile devices, and switch #body with #wrapper

--- app/helpers/jap_mag_widgets_helper.rb
+++ app/helpers/jap_mag_widgets_helper.rb
@@ -1,4 +1,21 @@
 module JapMagWidgetsHelper
+  
+  def is_mobile_device?
+    mobile_user_agents = 'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|webos|amoi|novarra|cdm|alcatel|pocket|iphone|mobileexplorer|mobile'
+    request.user_agent.to_s.downcase =~ Regexp.new(mobile_user_agents)
+  end
+  
+  def get_body_class
+    c = []
+    c << controller.controller_name
+    c << "mobile" if is_mobile_device?
+    
+    c.collect{|e| e.titleize.gsub(/\s/, "")}.join " "
+  end
+  
+  def get_body_id
+    content_for(:id).to_s.titleize.gsub(/\s/, "") unless content_for(:id).blank?
+  end
 
   #
   # navigation helper
