35e3bacbfc31be344097aca0c39788ef46d3fe82
Felix Ding <felixding@gmail.com>
2012-12-23 13:31:09 +0800

    move current_controller_action_in?() from app_controller to japmag module

--- lib/jap_mag.rb
+++ lib/jap_mag.rb
@@ -5,8 +5,26 @@ module JapMag
     t(key, options)
   end
   
+  def current_controller_action_in?(*args)
+    controller = params["controller"]
+    action = params["action"]
+
+    args.each do |element|
+      if element.include?("#")
+        array = element.match(/([a-z\-\_]*)#([a-z\-\_]*)/).to_a
+        c, a = array[1], array[2]
+        return true if controller == c && action == a
+      else
+        return true if controller == element
+      end
+    end
+  
+    false
+  end
+  
   def self.included base
     base.helper_method :_
+    base.helper_method :current_controller_action_in?
   end
   
   class Engine < Rails::Engine
